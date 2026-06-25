8b53bb83ffc4b62c8dfaaecd6a786c6876c6aa7b
vibratech2025 <jesusdelpiano@gmail.com>
2026-06-25 08:05:16 -0300

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'evo-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/logo.webp'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
